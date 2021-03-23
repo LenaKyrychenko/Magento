3cc82021d68dcbd3283561b67ab8f4a30c9ed8c5
OlenaKyrychenko <helen.kyrychenko.work@gmail.com>
2021-03-23 14:18:27 +0200

    style for home-top-slider

--- app/design/frontend/Elogic/base/requirejs-config.js
+++ app/design/frontend/Elogic/base/requirejs-config.js
@@ -1,7 +1,11 @@
 var config = {
+    deps: [
+        'js/theme'
+    ],
     paths: {
         slick:        'js/slick',
-        'elogicCoarousel': 'js/carousels'
+        'elogicCoarousel': 'js/carousels',
+        'elogicFirstSlider': 'js/firstDisplaySlider'
     },
     shim: {
         slick: {
