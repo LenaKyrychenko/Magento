4415a647bc01700b51475ab501a8ff2e3dd013c4
root <root@dockerized.site>
2021-04-29 14:32:53 +0000

    add page with controller

---
+++ app/code/Elogic/Gallery/view/frontend/web/js/gallery-slider.js
@@ -0,0 +1,32 @@
+define([
+    'jquery',
+    'jquery-ui-modules/widget',
+    'slick'
+], function ($){
+    'use strict';
+
+    $.widget('elogic.slider', {
+        options:{
+            template:"",
+            slickOptions:{
+                infinite: true,
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                centerPadding: '60px',
+                prevArrow: '<button class="slick-prev" aria-label="Prev" type="button">Prev</button>',
+                nextArrow: '<button class="slick-next" aria-label="Next" type="button">Next</button>'
+            }
+        },
+
+        _create: function (){
+            if(this.element.hasClass('slick-initialized')){
+                this.element.slick('unslick');
+            }
+            $(this.options.template).show();
+            $(this.options.template).slick(this.options.slickOptions);
+
+        }
+    });
+
+    return $.elogic.slider;
+});
